e5ac6b87efe4bbe8f950293ba2537205cbef68a9
胡斯凯 <67987406+SiberiaHusky@users.noreply.github.com>
2024-04-12 16:59:19 +0800

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-// 日期相关
+        // 日期相关
         var date = new Date();
 
         var todayDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
@@ -25,3 +25,15 @@
         setTimeout(function () {
             document.body.style.backgroundColor = "#1a237e";
         }, 5000);
+
+        // 获取所有带有 data-bgimg 属性的元素
+        var elements = document.querySelectorAll('[data-bgimg]');
+
+        // 循环处理每个元素
+        elements.forEach(function (element) {
+            // 获取 data-bgimg 属性的值
+            var imgUrl = element.getAttribute('data-bgimg');
+
+            // 将 data-bgimg 属性的值设置为背景图片
+            element.style.backgroundImage = 'url(' + imgUrl + ')';
+        });
